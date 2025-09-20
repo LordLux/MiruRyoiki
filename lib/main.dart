9702089e114f0c42059bfcfde1a4e2c7607f7c30
LordLux <74434026+LordLux@users.noreply.github.com>
2025-09-20 21:07:58 +0200

    refactor: Remove outdated TODO comment regarding player management

--- lib/main.dart
+++ lib/main.dart
@@ -1163,7 +1163,7 @@ Future<void> _registerWindowsUrlScheme(String scheme) async {
   }
 }
 
-// TODO only one player at a time
+// TODO detect video being watched
 // TODO move hidden series switches to settings
 // TODO view settings to choose what to show on homepage
 // TODO cache anime info
